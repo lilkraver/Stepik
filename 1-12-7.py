dbf21faa6549079074fed8333ca6de6bdc5466ec
lilkraver <88539664+lilkraver@users.noreply.github.com>
2021-10-24 19:45:00 +0700

    Update 1-12-7.py

--- 1-12-7.py
+++ 1-12-7.py
@@ -1,6 +1,6 @@
 a, b, c, d, e, f = input()
-n = int(a)+int (b)+int(c)
-m =int(d)+int (e)+int(f)
+n = int(a) + int(b) + int(c)
+m = int(d) + int(e) + int(f)
 if n == m:
     print ('Счастливый')
 else:

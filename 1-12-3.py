7a4758be61a29c0a9656986e7e849677e3152848
lilkraver <88539664+lilkraver@users.noreply.github.com>
2021-10-24 19:48:06 +0700

    Update and rename 1-12-3 to 1-12-3.py

---
+++ 1-12-3.py
@@ -0,0 +1,19 @@
+x = float(input())
+y = float(input())
+z = str(input())
+if(z == "+"):
+    print(x + y)
+elif(z == "-"):
+    print(x - y)
+elif(z == "*"):
+    print(x * y)
+elif(z == "pow"):
+    print(x ** y)
+elif(y == 0):
+    print("Деление на 0!")
+elif(z == "/"):
+    print(x / y)    
+elif(z == "mod"):
+    print(x % y)
+elif(z == "div"):
+    print(x // y)

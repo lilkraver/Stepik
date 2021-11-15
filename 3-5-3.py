e84723808d7de43f3c49c33e5d6501f61bd662be
lilkraver <88539664+lilkraver@users.noreply.github.com>
2021-11-15 13:54:59 +0700

    Update 3-5-3.py

--- 3-5-3.py
+++ 3-5-3.py
@@ -5,3 +5,12 @@ for i in range(1, len(sys.argv)):
     s = s + sys.argv[i] + ' '
 s2 = s
 print(s2, end=' ')
+
+
+import sys
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i], end=' ')
+
+    
+import sys
+print(' '.join(sys.argv[1:]))
